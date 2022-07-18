b03e862d9d69acd51d2c9c3c2776e84c792d9e6a
souravdutta8012 <souravdutta2108@gmail.com>
2022-07-18 21:16:42 +0530

    changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// ======================== 1. third min and third max ======================= //
+// 1. third min and third max ================================================ //
 
 const thirdRule = (arr) => {
     const a = arr.sort((a, b) => { return a - b })[2];
@@ -8,4 +8,16 @@ const thirdRule = (arr) => {
 
 console.log('1. ' + thirdRule([5, 6, 12, 3, 7, 13, 2]));
 
-// ======================== 1. third min and third max ======================= //
+// 2. curring in JS ========================================================== //
+
+const mul = (a) => {
+    return (b) => {
+        return (c) => {
+            return a * b * c;
+        }
+    }
+}
+
+console.log('2. ' + mul(2)(3)(4));
+
+// 3. curring in JS ========================================================== //
