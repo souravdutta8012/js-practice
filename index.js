1421f115813ed8f20ba861e36b9fc26f7d39c3de
souravdutta8012 <souravdutta2108@gmail.com>
2022-07-18 21:07:06 +0530

    changes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// ======================== third min and third max ======================= //
+// ======================== 1. third min and third max ======================= //
 
 const thirdRule = (arr) => {
-    const a = arr.sort(function (a, b) { return a - b })[2];
-    const b = (arr.sort(function (a, b) { return a - b }).reverse())[2];
+    const a = arr.sort((a, b) => { return a - b })[2];
+    const b = (arr.sort((a, b) => { return a - b }).reverse())[2];
     return [a, b];
 }
 
-console.log(thirdRule([5, 6, 12, 3, 7, 13, 2]));
+console.log('1. ' + thirdRule([5, 6, 12, 3, 7, 13, 2]));
+
+// ======================== 1. third min and third max ======================= //
